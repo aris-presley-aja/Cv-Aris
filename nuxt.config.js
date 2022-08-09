c38d4344ea2bc1fac9da2d111c888446851e1b88
aris22002-bitbucket <aris.22002.priyanto@gmail.com>
2022-08-09 14:12:07 +0700

    Push

---
+++ nuxt.config.js
@@ -0,0 +1,116 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+  
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Aris Priyanto - Frontend Web Developer',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Aris Priyanto is a Web Developer from Jakarta, Indonesia. After working as a freelancer for the past 3+ years, he is proficient at both frontend and backend development, but mostly im more like to work on Frontend.',
+      },
+      {
+        hid: 'keywords',
+        name: 'keywords',
+        content:
+          'Aris, Aris Priyanto, Jakarta, Frontend, Web Developer, PHP, JavaScript, Freelancer, Programmer, , Software Developer',
+      },
+      {
+        hid: 'author',
+        name: 'author',
+        content: 'Aris Priyanto',
+      },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-16x16.png',
+      },
+      {
+        rel: 'manifest',
+        href: '/site.webmanifest',
+      },
+    ],
+    script: [
+      {
+        // hid: 'tawk.to',
+        // src: 'https://embed.tawk.to/60ef6018d6e7610a49ab51bb/1fajguo58',
+        // defer: true,
+        crossorigin: '*',
+      },
+    ],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: ['@/scss'],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+    '@nuxtjs/svg-sprite',
+    '@nuxtjs/google-analytics',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/content
+    '@nuxt/content',
+    '@nuxtjs/sitemap',
+  ],
+
+  // Sitemap module options: https://sitemap.nuxtjs.org/guide/configuration
+  sitemap: {
+    hostname: 'https://vercel.com/aris-presley-aja/',
+    gzip: true,
+    routes: [
+      '/codefolio',
+      '/wid-blog-frontend',
+      '/habitly',
+      '/0xfolio',
+      '/react',
+      '/kotlin',
+      '/dream-flowers-design',
+    ],
+  },
+
+  // @nuxtjs/google-analytics configs, https://google-analytics.nuxtjs.org/setup/
+  googleAnalytics: {
+    id: 'UA-109228988-3',
+  },
+
+  // Content module configuration: https://go.nuxtjs.dev/config-content
+  content: {},
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
